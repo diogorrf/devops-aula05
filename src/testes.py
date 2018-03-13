34ab40b0e5f86bd5da3f132aba0f25e102755b52
diogorrf <36210618+diogorrf@users.noreply.github.com>
2018-03-12 22:22:17 -0300

    iden2
    iden2

--- src/testes.py
+++ src/testes.py
@@ -9,10 +9,10 @@ else:
     for linha in jogo:
         if len(linha) != 3:
             erroInicializar = True
-        else:
-            for elemento in linha:
-                if elemento != '.':
-                    erroInicializar = True
+    else:
+        for elemento in linha:
+            if elemento != '.':
+                erroInicializar = True
 if erroInicializar:
     sys.exit(1)
 else:
